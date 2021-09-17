113d7aaaa94c1795975e67e4b87414fec5fe4289
USWLM4qXURAyAa2h <USWLM4qXURAyAa2h@gmail.com>
2021-09-17 14:46:08 +0000

    start working on tests but realize I want to rework getDistance"

---
+++ src/test/java/edu/brown/cs/student/main/StarTest.java
@@ -0,0 +1,16 @@
+package edu.brown.cs.student.main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StarTest {
+  @Test
+  public void testSetGetDistances() {
+    Star s1 = new Star(1, "Star1", 1.1, 2.2, 3.3);
+    Star s2 = new Star(2, "Star2", 4.4, 5.5, 6.6);
+
+//    assertEquals(300303, output, 0.01);
+  }
+
+}
